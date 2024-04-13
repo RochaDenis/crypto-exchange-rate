21faca0c3144e4254f0039af495ef3b11741481f
Denis Rocha <dns_rochaa@hotmail.com>
2024-04-13 19:41:19 -0300

    set apikey

--- crypto-exchange-rate/src/components/fetchData/fetchData.jsx
+++ crypto-exchange-rate/src/components/fetchData/fetchData.jsx
@@ -9,7 +9,7 @@ function fetchData(fromCurrency, toCurrency){
         url: 'https://alpha-vantage.p.rapidapi.com/query', // URL da API
         params: { from_currency: fromCurrency, function: 'CURRENCY_EXCHANGE_RATE', to_currency: toCurrency }, // Parâmetros da requisição
         headers: {
-            'X-RapidAPI-Key': '1c762af8b8msh6da4ded8ba7e011p146afbjsn97d2181f796f', // Chave de autenticação da API
+            'X-RapidAPI-Key': 'Your APIKEY', // Chave de autenticação da API
             'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com' // Host da API
           }
     }; 
